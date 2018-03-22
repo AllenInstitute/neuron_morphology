ffe7b46bbbf4b2e32affd774973ed0b19a0b477a
Nika Hejazinia <nikah@alleninstitute.org>
2018-03-22 15:16:00 -0700

    PBS-1166 updating the arguments to match _schemas

--- bin/run_reconstruction_tile_viewer.py
+++ bin/run_reconstruction_tile_viewer.py
@@ -8,8 +8,8 @@ import allensdk.neuron_morphology.rendering.reconstruction_tile_viewer as mv
 def main():
 
     module = ags.ArgSchemaParser(schema_type=ReconstructionTileViewerParameters)
-    mv.create_tile_viewer(module.args["morphology_csv_file"],
-                          module.args["html_file"],
+    mv.create_tile_viewer(module.args["morphology_csv_input_file"],
+                          module.args["html_output_file"],
                           module.args["reconstruction_hierarchy"],
                           module.args["reconstruction_card_properties"],
                           module.args["max_columns"])
