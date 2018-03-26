bf716acfc362668c123852caf16f8f4c270b0706
Nika Hejazinia <nikah@alleninstitute.org>
2018-03-26 13:58:31 -0700

    adding build.sh and test.sh

---
+++ .bamboo/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -eu
+export PATH=/shared/utils.x86_64/anaconda2-4.3.1/bin:$PATH
+export TMPDIR=${bamboo_build_working_directory}/.tmp
+export CONDA_PATH_BACKUP=${CONDA_PATH_BACKUP:-$PATH}
+export CONDA_PREFIX=${CONDA_PREFIX:-}
+export CONDA_PS1_BACKUP=${CONDA_PS1_BACKUP:-}
+source activate ${bamboo_build_working_directory}/.conda/${bamboo_TEST_ENVIRONMENT}
+cd ${bamboo_build_working_directory}/${bamboo_CHECKOUT_DIRECTORY}
+python setup.py bdist_wheel
+source deactivate
---
+++ .bamboo/test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -eu
+export PATH=/shared/utils.x86_64/anaconda2-4.3.1/bin:$PATH
+export HOME=${bamboo_build_working_directory}/.home
+export TMPDIR=${bamboo_build_working_directory}
+export CONDA_PATH_BACKUP=${CONDA_PATH_BACKUP:-$PATH}
+export CONDA_PREFIX=${CONDA_PREFIX:-}
+export CONDA_PS1_BACKUP=${CONDA_PS1_BACKUP:-}
+conda create -y -${bamboo_VERBOSITY} --clone ${bamboo_BASE_ENVIRONMENT} --prefix ${bamboo_build_working_directory}/.conda/${bamboo_TEST_ENVIRONMENT}
+source activate ${bamboo_build_working_directory}/.conda/${bamboo_TEST_ENVIRONMENT}
+cd ${bamboo_build_working_directory}/${bamboo_CHECKOUT_DIRECTORY}
+pip install -r requirements.txt
+pip install -r requirements_test.txt
+pip install .
+pytest --junitxml=test-reports/tests.xml
+coverage run --source ./ -m pytest
+coverage html --omit="test/*,setup.py"
+source deactivate
