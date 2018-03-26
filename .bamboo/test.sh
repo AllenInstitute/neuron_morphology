3e8c94dda4ae905685247ddb1903f4edbe47b2cf
Nika Hejazinia <nikah@alleninstitute.org>
2018-03-26 14:59:49 -0700

    removing an existing environment before creating one

--- .bamboo/test.sh
+++ .bamboo/test.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 set -eu
 export PATH=/shared/utils.x86_64/anaconda2-4.3.1/bin:$PATH
 export HOME=${bamboo_build_working_directory}/.home
@@ -6,13 +7,17 @@ export TMPDIR=${bamboo_build_working_directory}
 export CONDA_PATH_BACKUP=${CONDA_PATH_BACKUP:-$PATH}
 export CONDA_PREFIX=${CONDA_PREFIX:-}
 export CONDA_PS1_BACKUP=${CONDA_PS1_BACKUP:-}
-conda create -y -${bamboo_VERBOSITY} --clone ${bamboo_BASE_ENVIRONMENT} --prefix ${bamboo_build_working_directory}/.conda/${bamboo_TEST_ENVIRONMENT}
-source activate ${bamboo_build_working_directory}/.conda/${bamboo_TEST_ENVIRONMENT}
-cd ${bamboo_build_working_directory}/${bamboo_CHECKOUT_DIRECTORY}
-pip install -r requirements.txt
-pip install -r requirements_test.txt
-pip install .
-pytest --junitxml=test-reports/tests.xml
-coverage run --source ./ -m pytest
-coverage html --omit="test/*,setup.py"
+CONDA_TEST_ENV_PREFIX=${bamboo_build_working_directory}/.conda/${bamboo_TEST_ENVIRONMENT}
+if [ -d ${CONDA_TEST_ENV_PREFIX} ]; then
+    conda remove -y -${bamboo_VERBOSITY} --prefix ${CONDA_TEST_ENV_PREFIX} --all
+fi
+conda create -y -${bamboo_VERBOSITY} --clone ${bamboo_BASE_ENVIRONMENT} --prefix ${CONDA_TEST_ENV_PREFIX} &&
+source activate ${bamboo_build_working_directory}/.conda/${bamboo_TEST_ENVIRONMENT} &&
+cd ${bamboo_build_working_directory}/${bamboo_CHECKOUT_DIRECTORY} &&
+pip install -r requirements.txt &&
+pip install -r requirements_test.txt &&
+pip install . &&
+pytest --junitxml=test-reports/tests.xml &&
+coverage run --source ./ -m pytest &&
+coverage html --omit="test/*,setup.py" &&
 source deactivate
