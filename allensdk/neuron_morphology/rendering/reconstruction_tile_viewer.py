87d35b88123d4f1118e737b57c00223893a6f4df
Nika Hejazinia <nikah@alleninstitute.org>
2018-04-09 23:30:13 -0700

    adding is_thumbnail

--- allensdk/neuron_morphology/rendering/reconstruction_tile_viewer.py
+++ allensdk/neuron_morphology/rendering/reconstruction_tile_viewer.py
@@ -3,6 +3,7 @@ from jinja2 import Environment, select_autoescape, FileSystemLoader
 from allensdk.neuron_morphology.rendering.reconstruction_grouping import create_reconstruction_grouping
 import os
 import sys
+import imghdr
 
 
 def parse_csv(csv_file):
@@ -20,17 +21,38 @@ def parse_csv(csv_file):
         data = list(reader)
     return data
 
+
 def get_template_path(template_dir_name):
 
     """Gets the jinja2 template directory path. This function is necessary for running this module in a bundle.
-       PyInstaller creates a temp folder and stores the path in sys._MEIPASS when this package is bundled. 
+       PyInstaller creates a temp folder and stores the path in sys._MEIPASS when this package is bundled.
        When not run in the bundle, the path is the template folder in the current directory.
-       
+
     :parameter template_dir_name: string
                 name of the template directory
     """
     base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
     return os.path.join(base_path, template_dir_name)
+
+
+def is_thumbnail(key, value):
+
+    """Determines if the key and value of a dictionary item is a thumbnail
+
+    :parameter key: string
+    :parameter value: string
+
+    :return True or False: boolean
+    """
+
+    is_thumbnail = False
+    if key.endswith('thumbnail'):
+        is_thumbnail = True
+    elif os.path.isfile(value) and imghdr.what(value):
+        is_thumbnail = True
+
+    return is_thumbnail
+
 
 def create_tile_viewer(csv_file, html_file, reconstruction_hierarchy, reconstruction_card_properties, max_columns=None):
 
@@ -51,11 +73,12 @@ def create_tile_viewer(csv_file, html_file, reconstruction_hierarchy, reconstruc
     """
     template_dir = get_template_path('templates')
     env = Environment(loader=FileSystemLoader(template_dir), autoescape=select_autoescape(['html', 'xml']))
+    env.filters['is_thumbnail'] = is_thumbnail
     html_template = env.get_template('reconstruction_card.html')
     data = parse_csv(csv_file)
     reconstruction_grouping = create_reconstruction_grouping(reconstruction_hierarchy, reconstruction_card_properties,
                                                              data)
-    reconstruction_card = set().union(item['attribute'] for item in reconstruction_card_properties)
+    reconstruction_card = [item['attribute'] for item in reconstruction_card_properties]
     if max_columns:
         max_columns = int(max_columns)
 
