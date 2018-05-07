e370a093f8828a8b9f3f43c9cc9f5a9b5244e9e3
Nika Hejazinia <nikah@alleninstitute.org>
2018-05-07 13:45:48 -0700

    adding Pipfile and tox

--- setup.py
+++ setup.py
@@ -5,24 +5,17 @@ import os
 
 scripts = glob.glob(os.path.join('bin', '*'))
 
-with open('requirements.txt', 'r') as f:
-    requirements = f.read().splitlines()
-
-with open('requirements_test.txt', 'r') as f:
-    requirements_test = f.read().splitlines()
 
 setup(
     version='0.2.4',
     name='allensdk_neuron_morphology',
-    author='Nike Keller, Keith Godfrey',
+    author='Nika Keller, Keith Godfrey',
     author_email='nikah@alleninstitute.org',
     packages=find_packages(),
     include_package_data=True,
     scripts=scripts,
     description='Neuron morphology analysis and visualization tools',
-    install_requires=requirements,
-    tests_require=requirements_test,
-    setup_requires=['setuptools', 'sphinx', 'numpydoc'],
+    setup_requires=['pytest-runner'],
     entry_points={
           'console_scripts': [
               'allensdk.neuron_morphology = allensdk.neuron_morphology.__main__:main'
