95a1f8f02f33fc53e63d62dd87ecc9658a0d0913
nile graddis <NileGraddis@gmail.com>
2019-12-20 15:26:14 -0800

    #1261 docstrings for feature extractor

--- neuron_morphology/feature_extractor/data.py
+++ neuron_morphology/feature_extractor/data.py
@@ -1,10 +1,13 @@
 from neuron_morphology.morphology import Morphology
 
 class Data:
-    """ A placeholder
-    """
-    
+
     def __init__(self, morphology: Morphology, **other_things):
+        """ A placeholder for the "general data" objsect that we will pass 
+        into the feature extractor. Guaranteed to have a morphology. Might 
+        have other things.
+        """
+
         self.morphology: Morphology = morphology
         for name, value in other_things.items():
             setattr(self, name, value)
--- neuron_morphology/feature_extractor/mark.py
+++ neuron_morphology/feature_extractor/mark.py
@@ -1,14 +1,31 @@
 from neuron_morphology.feature_extractor.data import Data
 
 class Mark:
+    """ A tag, intended for use in feature selection.
+    """
 
     def validate(self, data: Data) -> bool:
+        """ Determine if this feature is calculable from the provided data.
+
+        Parameters
+        ----------
+        data : Data from a single morphological reconstruction
+
+        Returns
+        -------
+        whether marked features can be calculated from these data
+
+        """
+
         return True
 
 
 class RequiresLayerAnnotations(Mark):
 
     def validate(self, data: Data) -> bool:
+        """ Checks whether each node in the data's morphology is annotated with 
+        a cortical layer. Returns False if any are missing.
+        """
 
         has_layers = True
         for node in data.morphology.nodes:
