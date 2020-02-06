50688f1f53615790c56cea4ba28c131a4293beb9
MatthewAitken <matthewaitken2@gmail.com>
2020-02-06 08:57:55 -0800

    adding affine transformer

---
+++ neuron_morphology/transforms/affine_transformer/apply_affine_transform.py
@@ -0,0 +1,38 @@
+import sys
+
+from argschema import ArgSchemaParser
+
+from neuron_morphology.swc_io import morphology_from_swc, write_swc
+from neuron_morphology.transforms.affine_transformer._schemas import (
+    ApplyAffineSchema, OutputParameters)
+from neuron_morphology.transforms.affine_transform import AffineTransform
+
+
+def main():
+    mod = ArgSchemaParser(schema_type=ApplyAffineSchema,
+                          output_schema_type=OutputParameters)
+    args = mod.args
+
+    if 'affine_dict' in args:
+        affine_transform = AffineTransform.from_dict(args['affine_dict'])
+    elif 'affine_list' in args:
+        affine_transform = AffineTransform.from_list(args['affine_list'])
+    else:
+        raise ValueError('must provide either an affine_dict or affine_list')
+
+    morph_in = morphology_from_swc(args['input_swc'])
+
+    morph_out = affine_transform.transform_morphology(morph_in)
+
+    write_swc(morph_out, args['output_swc'])
+
+    output = {
+        'inputs': args,
+        'output_swc': args['output_swc'],
+    }
+
+    mod.output(output)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
