bd2844dca40d8e643a01c935df2fb33b87ec52bf
Matt Aitken <matthewaitken2@gmail.com>
2020-02-10 17:07:08 -0800

    20/streamline calculation (#41)
    Adds a module to calculate streamlines
    
    * initial streamline code
    
    * updates streamline outputs and adding tests
    
    * adding module code
    
    * adds translation vector to outputs
    
    * skips tests requiring fenics
    
    * 20/streamline conda ci (#52)
    
    updates circleci to run fenics tests in conda
    
    * 20/streamline conda ci (#53)
    
    * changes python version to 3.8
    
    * adds shapely to requirements (only used for fenics/streamline right now...)
    
    * updates ccw vertices doc, seperates simple polygon vertices creation from ccw check, adds convenience function to do both
    
    * changes udot to grad_u
    
    * renaming udot to grad_u in calculate_pia_wm_streamlines to match naming convention
    
    * speeds up grid calculation by using scipy.interpolate.griddata (#54)
    
    * speeds up grid calculation by using scipy.interpolate.griddata
    
    * changes mesh_values and mesh_gradients calculation in generate_laplace_field to use compute_vertex_values
    
    * updates docstrings for laplace functions

---
+++ neuron_morphology/transforms/pia_wm_streamlines/_schemas.py
@@ -0,0 +1,54 @@
+from argschema import ArgSchema
+from argschema.schemas import DefaultSchema
+from argschema.fields import (
+    Nested, String, Int, Float, OutputDir, OutputFile, NumpyArray)
+
+
+class PiaWmStreamlineSchema(ArgSchema):
+    """Arg Schema for run_pia_wm_streamlines"""
+
+    pia_path_str = String(
+        required=True,
+        description='string alternating x, y coordinates outlining the pia')
+    wm_path_str = String(
+        required=True,
+        description='string alternating x, y coordinates outlining the wm')
+    soma_path_str = String(
+        required=False,
+        description='string alternating x, y coordinates outlining the soma. '
+                    'If provided, streamlines will be translated so that '
+                    'the origin is at the soma')
+    resolution = Float(required=False,
+                       default=1,
+                       description='Resolution of pixels in microns')
+    pia_fixed_value = Float(required=False,
+                            default=1,
+                            description='Fixed value pia boundary condition')
+    wm_fixed_value = Float(required=False,
+                           default=0,
+                           description='Fixed value wm boundary condition')
+    mesh_res = Int(required=False,
+                   default=20,
+                   description='Resolution for mesh for laplace solver')
+    output_dir = OutputDir(required=True,
+                           description='Directory to write xarray results')
+    output_json = OutputFile(required=False,
+                             description='output json file path')
+
+
+class OutputParameters(DefaultSchema):
+    inputs = Nested(
+        PiaWmStreamlineSchema,
+        description="The parameters argued to this executable",
+        required=True
+    )
+    depth_field_file = OutputFile(
+        required=True,
+        description='location of depth field xarray')
+
+    gradient_field_file = OutputFile(
+        required=True,
+        description='location of gradient field xarray')
+    translation = NumpyArray(
+        required=False,
+        description='translation if applied')
