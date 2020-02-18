9f2dd70a55be0e19e9a144f72b5f2807db96bf13
MatthewAitken <matthewaitken2@gmail.com>
2020-02-18 10:04:18 -0800

    matches LICENSE file to AllenInstitute/ghinfo/LICENSE, and removes allensdk from marker.py

--- neuron_morphology/marker.py
+++ neuron_morphology/marker.py
@@ -1,18 +1,3 @@
-# Copyright 2015-2016 Allen Institute for Brain Science
-# This file is part of Allen SDK.
-#
-# Allen SDK is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, version 3 of the License.
-#
-# Allen SDK is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Allen SDK.  If not, see <http://www.gnu.org/licenses/>.
-
 import csv
 import neuron_morphology.constants as constants
 from neuron_morphology.validation.result import InvalidMarkerFile
