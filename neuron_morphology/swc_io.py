d34d219c291e2637bc08884c02e8adbe10e604d9
MattAitken <matthewaitken2@gmail.com>
2020-03-13 10:01:24 -0700

    debug windows swc_io

--- neuron_morphology/swc_io.py
+++ neuron_morphology/swc_io.py
@@ -32,6 +32,7 @@ def write_swc(data, path, comments=None, sep=' ', columns=SWC_COLUMNS, casts=COL
     apply_casts(data, casts)
 
     data = data[[col for col in columns]]
+    print(data)
     with open(path, 'w') as swc_file:
         swc_file.writelines(comments)
         data.to_csv(swc_file, sep=sep, index=False, header=None)
