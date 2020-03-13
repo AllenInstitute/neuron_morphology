5f0f0550779c88282dc966fdadbf86bbabc44599
MattAitken <matthewaitken2@gmail.com>
2020-03-13 11:30:55 -0700

    windows swcio debug

--- tests/test_swc_io.py
+++ tests/test_swc_io.py
@@ -34,13 +34,16 @@ class TestSWCIO(unittest.TestCase):
     def test_create_morphology_from_swc(self):
         morph = swcio.morphology_from_swc(self.swc_file)
         self.assertEqual(morph.get_root()['parent'], -1)
+        self.assertEqual(morph.node_by_id(2)['parent'], 1)
 
     def test_save_morphology_to_swc(self):
         test_swc_path = os.path.join(self.test_dir, 'test.swc')
         swcio.morphology_to_swc(self.morphology, test_swc_path)
 
         with open(test_swc_path, 'r') as test_swc:
-            line = test_swc.readline().rstrip().split(' ')
+            line = test_swc.readline()
+            print(line)
+            line = line.rstrip().split(' ')
             print(line)
             self.assertEqual(int(line[-1]), -1)
             line = test_swc.readline().rstrip().split(' ')
