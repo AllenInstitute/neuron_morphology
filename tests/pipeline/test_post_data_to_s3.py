787f119c19177fcc585f41f731ebc75dd0119785
gnayuy <gnayuy@gmail.com>
2020-04-29 02:46:54 -0700

    add unit test for zip_files func

--- tests/pipeline/test_post_data_to_s3.py
+++ tests/pipeline/test_post_data_to_s3.py
@@ -3,6 +3,7 @@ import os
 import boto3
 import zipfile
 from moto import mock_s3
+import unittest
 
 from neuron_morphology.pipeline.post_data_to_s3 import post_object_to_s3, zip_files
 
@@ -19,6 +20,20 @@ file_dict = {file_name1: file_path1, file_name2: file_path2}
 bucket_name = "neuronmorphologypipeline"
 archive_name = "test_post_data.zip"
 region = "us-west-2"
+
+class TestZipFiles(unittest.TestCase):
+    def setUp(self):
+        self.mock_listing = [file_name1, file_name2]
+        self.file_dict = file_dict
+
+    def test_zip_files(self):
+        
+        archive = zip_files(self.file_dict)
+
+        if zipfile.is_zipfile(archive): 
+            file_list = [file_name for file_name in zipfile.ZipFile(archive).namelist()]
+
+        self.assertListEqual(self.mock_listing, file_list)
 
 @mock_s3
 def test_post_data_to_s3(tmpdir_factory):
