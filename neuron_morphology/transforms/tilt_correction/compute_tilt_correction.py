f08827222e6256ca03197627de64e3d16c25792f
MattAitken <matthewaitken2@gmail.com>
2020-05-04 10:46:22 -0700

    finishes and adds tests for tilt correction module

--- neuron_morphology/transforms/tilt_correction/compute_tilt_correction.py
+++ neuron_morphology/transforms/tilt_correction/compute_tilt_correction.py
@@ -4,74 +4,141 @@ import logging
 import copy as cp
 
 import numpy as np
+from scipy.spatial.distance import euclidean
 import pandas as pd
+import h5py
 
 from argschema.argschema_parser import ArgSchemaParser
-from neuron_morphology.transforms.scale_correction._schemas import (
+from neuron_morphology.transforms.tilt_correction._schemas import (
     InputParameters, OutputParameters)
 from neuron_morphology.morphology import Morphology
 from neuron_morphology.swc_io import morphology_from_swc
 import neuron_morphology.transforms.affine_transform as aff
 
 
+CCF_SHAPE = (1320, 800, 1140)
+CCF_RESOLUTION = 10
+
+
 def get_tilt_correction(morphology: Morphology,
-                        soma_marker: Dict,
-                        slice_angle: float,
-                        streamlines,
+                        soma_voxel: Dict[str, int],
+                        slice_angle_matrix: float,
+                        closest_path,
                         ):
     """
-        Estimate a scale factor correction from recorded cell depth
+        Find the tilt angle between the slice plane and the nearest streamline
 
         Parameters
         ----------
         morphology: Morphology object
-        soma_marker: soma marker from revised marker file
-        slice_angle: angle the slice was cut at about x axis
-        streamlines: data structure containing ccf streamlines
+        soma_voxel: soma voxel in ccf {'x': , 'y': , 'z': }
+        slice_angle_matrix: 4 x 4 affine matrix of the slice plane
+            relative to ccf
+        closest_path: 3 x N array of voxel coordinates in closest streamline
 
         Returns
         -------
-        tilt angle correction
+        tilt angle correction (radians)
 
     """
-    
+
+    # Find slice plane vector
+    M = slice_angle_matrix[0:3, :]
+    a2 = np.dot(M, np.array([0, 0, 0, 1]))
+    b2 = np.dot(M, np.array([1, 0, 0, 1]))
+    c2 = np.dot(M, np.array([0, 1, 0, 1]))
+
+    norm_vec = np.cross(b2 - a2, c2 - a2)
+    norm_unit = norm_vec / euclidean(norm_vec, [0, 0, 0])
+
+    # Find approximate streamline vector
+    if euclidean(closest_path[:, 0], soma_voxel) == 0:
+        # if soma is at the top, calculate from other end of streamline
+        streamline_unit = (soma_voxel - closest_path[:, -1]) /\
+            euclidean(soma_voxel, closest_path[:, -1])
+    else:
+        streamline_unit = (closest_path[:, 0] - soma_voxel) /\
+             euclidean(closest_path[:, 0], soma_voxel)
+
+    # determine angle between
+    s = np.linalg.norm(np.cross(norm_unit, streamline_unit))
+    dp = np.dot(norm_unit, streamline_unit)
+    theta = np.arctan2(s, dp)
+
+    tilt_angle = np.pi / 2 - theta
+
     return tilt_angle
 
 
-def get_soma_marker_from_marker_file(marker_path: str):
-    col_names = ['x', 'y', 'z', 'radius', 'shape', 'name',
-                 'comment', 'color_r', 'color_g', 'color_b']
-    markers = pd.read_csv(marker_path, sep=',', comment='#',
-                          header=None, names=col_names)
-    soma_marker = markers.loc[markers['name'] == 30].to_dict('r')
-    return soma_marker[0]
+def load_path_ids_and_voxels(ccf_path):
+    vi = h5py.File(ccf_path, 'r')
+
+    # initialize
+    path_lookup_table = vi['view lookup'][:][np.where(vi['view lookup'][:] > -1)]
+    paths = vi['paths'][:]
+    vi.close()
+
+    voxel_idxs = []
+    path_id_from_voxel_idx = {}
+
+    for path_id in path_lookup_table:
+        path = paths[path_id, :]
+        path = path[path > 0]
+        for voxel in path:
+            path_id_from_voxel_idx[voxel] = [path_id]
+        voxel_idxs.extend(path)
+
+    return paths, path_id_from_voxel_idx, voxel_idxs
 
 
-def load_streamlines(ccf_path):
-    return streamlines
+def find_closest_path_id(soma_voxel: Dict[str, int],
+                         path_id_from_voxel_idx: Dict[int, int],
+                         voxel_idxs: List[int]):
+
+    voxels = np.array(np.unravel_index(voxel_idxs, CCF_SHAPE))
+    delta = voxels - np.reshape(soma_voxel, (3, 1))
+    distances = np.linalg.norm(delta, axis=0)
+    min_voxel = voxels[:, distances.argmin()]
+    min_voxel_idx = np.ravel_multi_index(min_voxel, CCF_SHAPE)
+    closest_path_id = path_id_from_voxel_idx[min_voxel_idx]
+
+    return closest_path_id
 
 
 def run_tilt_correction(
         swc_path: str,
-        marker_path: str,
-        slice_angle: float,
+        ccf_soma_location: Dict,
+        slice_transform: aff.AffineTransform,
         ccf_path: str):
 
     morph = morphology_from_swc(swc_path)
-    soma_marker = get_soma_marker_from_marker_file(marker_path)
-    streamlines = load_streamlines(ccf_path)
+
+    (paths, path_id_from_voxel_idx, voxel_idxs) \
+        = load_path_ids_and_voxels(ccf_path)
+
+    soma_voxel = (int(ccf_soma_location["x"] // CCF_RESOLUTION),
+                  int(ccf_soma_location["y"] // CCF_RESOLUTION),
+                  int(ccf_soma_location["z"] // CCF_RESOLUTION))
+
+    closest_path_id = find_closest_path_id(soma_voxel,
+                                           path_id_from_voxel_idx,
+                                           voxel_idxs)
+
+    closest_path = paths[closest_path_id]
+    closest_path = closest_path[closest_path > 0]
+    closest_path_coords = np.array(np.unravel_index(closest_path, CCF_SHAPE))
 
     tilt = get_tilt_correction(morph,
-                               soma_marker,
-                               slice_angle,
-                               streamlines)
+                               soma_voxel,
+                               slice_transform.affine,
+                               closest_path_coords)
 
     transform = aff.affine_from_transform(
                     aff.rotation_from_angle(tilt, axis=0))
 
     output = {
         'tilt_transform_dict': aff.AffineTransform(transform).to_dict(),
-        'tilt_angle': str(tilt)
+        'tilt_correction': str(tilt)
     }
     return output
 
@@ -85,10 +152,20 @@ def main():
     args = cp.deepcopy(parser.args)
     logging.getLogger().setLevel(args.pop("log_level"))
 
+    if 'slice_transform_dict' in args:
+        slice_transform = aff.AffineTransform.from_dict(
+            args['slice_transform_dict'])
+    elif 'slice_transform_list' in args:
+        slice_transform = aff.AffineTransform.from_list(
+            args['slice_transform_list'])
+    else:
+        raise ValueError('must provide either an slice_transform_dict '
+                         'or an slice_transform_list')
+
     output = run_tilt_correction(
         args["swc_path"],
-        args["marker_path"],
-        args["slice_transform_list"],
+        args["ccf_soma_location"],
+        slice_transform,
         args["ccf_path"],
     )
     output.update({"inputs": parser.args})
--- tests/transforms/test_tilt_correction.py
+++ tests/transforms/test_tilt_correction.py
@@ -5,11 +5,12 @@ import shutil
 import tempfile
 
 import numpy as np
+import h5py
 
 from neuron_morphology.morphology_builder import MorphologyBuilder
 from neuron_morphology.swc_io import morphology_to_swc
 from neuron_morphology.transforms.tilt_correction.compute_tilt_correction import (
-    get_tilt_correction)
+    get_tilt_correction, CCF_SHAPE, CCF_RESOLUTION)
 from neuron_morphology.transforms.affine_transform import AffineTransform
 
 import allensdk.core.json_utilities as ju
@@ -30,22 +31,27 @@ class TestScaleCorrection(unittest.TestCase):
         self.swc_path = os.path.join(self.test_dir, '123.swc')
         morphology_to_swc(self.morphology, self.swc_path)
 
-        self.marker_path = os.path.join(self.test_dir, '123.marker')
-        with open(self.marker_path, 'w') as f:
-            f.write("##x,y,z,radius,shape,name,comment, color_r,color_g,color_b\n"
-                    "1.0,2.0,3.5,0.0,0,30,0,255, 255, 255\n"
-                    "0.0,2.0,3.0,0.0,0,20,0,255, 255, 255")
+        self.slice_transform_list = [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0]
+        self.slice_transform = AffineTransform.from_list(self.slice_transform_list).affine
 
-        self.soma_marker = {'x': 1.0, 'y': 2.0, 'z': 3.5}
+        self.soma_voxel = (400, 400, 1120)
 
-        self.slice_transform = 
+        self.ccf_soma_location = {'x': self.soma_voxel[0] * CCF_RESOLUTION,
+                                  'y': self.soma_voxel[1] * CCF_RESOLUTION,
+                                  'z': self.soma_voxel[2] * CCF_RESOLUTION
+                                  }
 
-        self.streamlines = 
+        # Streamline is on z+, so should result in tilt of np.pi / 2
+        self.closest_path = np.asarray([400 * np.ones((20,), dtype='i'),
+                                        400 * np.ones((20,), dtype='i'),
+                                        np.arange(1120, 1140, dtype='i')])
 
-        self.ccf_path = 
+        self.ccf_path = os.path.join(self.test_dir, 'paths.h5')
+        path = np.ravel_multi_index(self.closest_path, CCF_SHAPE)
+        with h5py.File(self.ccf_path, 'w') as f:
+            f.create_dataset("view lookup", (1, ), dtype='i', data=0)
+            f.create_dataset("paths", (1, 20), dtype='i', data=path)
 
-
-        
         self.output_json_path = os.path.join(self.test_dir, 'output.json')
 
     def tearDown(self):
@@ -53,14 +59,16 @@ class TestScaleCorrection(unittest.TestCase):
 
     def test_tilt_correction(self):
         tilt = get_tilt_correction(self.morphology,
-                                   self.soma_marker,
+                                   self.soma_voxel,
                                    self.slice_transform,
-                                   self.streamlines)
-        self.assertAlmostEqual(tilt, )
+                                   self.closest_path)
+        self.assertAlmostEqual(tilt, -np.pi / 2)
 
     def test_scale_correction_end_to_end(self):
         input = {'swc_path': self.swc_path,
-                 'marker_path': self.marker_path,
+                 'ccf_soma_location.x': self.ccf_soma_location['x'],
+                 'ccf_soma_location.y': self.ccf_soma_location['y'],
+                 'ccf_soma_location.z': self.ccf_soma_location['z'],
                  'slice_transform_list': self.slice_transform_list,
                  'ccf_path': self.ccf_path,
                  'output_json': self.output_json_path}
@@ -73,12 +81,12 @@ class TestScaleCorrection(unittest.TestCase):
         sp.check_call(cmd)
 
         outputs = ju.read(self.output_json_path)
-        self.assertAlmostEqual(outputs['tilt_correction'], )
+        self.assertAlmostEqual(outputs['tilt_correction'], -np.pi / 2)
 
         aff_t = AffineTransform.from_dict(outputs['tilt_transform_dict'])
         morph_t = aff_t.transform_morphology(self.morphology)
 
         axon = morph_t.node_by_id(1)
         self.assertAlmostEqual(axon['x'], 0)
-        self.assertAlmostEqual(axon['y'], 2)
-        self.assertAlmostEqual(axon['z'], 6)
+        self.assertAlmostEqual(axon['y'], 3)
+        self.assertAlmostEqual(axon['z'], -2)
