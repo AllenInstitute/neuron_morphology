741023c963033dc0869d71b48a02862e43a993e7
nilegraddis <nilegraddis@gmail.com>
2020-05-08 11:23:31 -0700

    #118 landing task

--- pipeline/commands/landing.py
+++ pipeline/commands/landing.py
@@ -1,24 +1,57 @@
 import os
 import traceback
 import json
+import zipfile
+import io
+import uuid
 
 import boto3
 
 
 step_fns = boto3.client("stepfunctions")
+s3 = boto3.client("s3")
+
+
+def extract_reconstruction_id(key):
+    return key.split("/")[-1].split(".")[0]
 
 
 def main():
     
-    token = os.environ.get("TASK_TOKEN")
+    token = os.environ["TASK_TOKEN"]
     
     try:
 
-        working_bucket = os.environ.get("WORKING_BUCKET")
-        landing_bucket = os.environ.get("LANDING_BUCKET")
-        upload_package_key = os.environ.get("UPLOAD_PACKAGE_KEY")
+        working_bucket = os.environ["WORKING_BUCKET"]
+        landing_bucket = os.environ["LANDING_BUCKET"]
+        upload_package_key = os.environ["UPLOAD_PACKAGE_KEY"]
 
-        output = {"foo": "bar"}
+        run_id = uuid.uuid4()
+        reconstruction_id = extract_reconstruction_id(upload_package_key)
+        base_key = f"{reconstruction_id}/{run_id}"
+
+        upload_package_response = s3.get_object(
+            Bucket=landing_bucket, Key=upload_package_key
+        )
+
+        archive = zipfile.ZipFile(
+            io.BytesIO(
+                upload_package_response["Body"].read()
+            )
+        )
+
+        for name in archive.namelist():
+            s3.put_object(
+                Body=archive.read(name),
+                Key=f"{base_key}/{name}"
+            )
+
+        s3.delete_object(Bucket=landing_bucket, Key=upload_package_key)
+
+        output = {
+            "base_key": base_key,
+            "bucket_name": working_bucket
+        }
 
     except Exception as err:
         print(err)
@@ -31,7 +64,7 @@ def main():
         raise
 
     step_fns.send_task_success(
-        token,
+        taskToken=token,
         output=json.dumps(output)
     )
 
