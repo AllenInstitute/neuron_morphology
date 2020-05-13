911141d4023168ae25d19df35c443150a864efda
MattAitken <matthewaitken2@gmail.com>
2020-05-13 16:29:31 -0700

    Revert "adds debug logging to tilt command"
    This reverts commit c61353bb88e319f874103ac28be41df35f2b946c.

--- pipeline/commands/tilt.py
+++ pipeline/commands/tilt.py
@@ -2,8 +2,6 @@ import os
 import json
 from io import BytesIO
 
-import logging
-
 import boto3
 
 from neuron_morphology.transforms.tilt_correction.compute_tilt_correction \
@@ -16,8 +14,6 @@ from harness import step_fns_ecs_harness
 
 
 s3 = boto3.client("s3")
-logger = logging.getLogger('simple_example')
-logger.setLevel(logging.DEBUG)
 
 
 def collect_inputs(working_bucket, run_prefix,
@@ -33,7 +29,6 @@ def collect_inputs(working_bucket, run_prefix,
     # Use input swc for now
     upright_swc_key = f"{run_prefix}/{metadata['swc_file']}"
 
-    logger.debug('Collecting S3 Objects')
     swc_response = s3.get_object(Bucket=working_bucket,
                                  Key=upright_swc_key,
                                  )
@@ -45,13 +40,10 @@ def collect_inputs(working_bucket, run_prefix,
                                  Key=ccf_key,
                                  )
 
-    logger.debug('Loading morphology')
     morphology = morphology_from_swc(swc_response["Body"])
-    logger.debug('Loading marker file')
     soma_marker = read_soma_marker(marker_response["Body"])
-    logger.debug('Loading ccf_data')
     ccf_data = load_ccf_data(BytesIO(ccf_response["Body"].read()))
-    logger.debug('Loading slice_transform')
+
     slice_transform = AffineTransform.from_dict(metadata['slice_transform'])
 
     return {
@@ -68,12 +60,9 @@ def put_outputs(bucket, prefix,
                 tilt_correction, tilt_transform, transformed_morphology):
 
     transformed_swc_key = f"{prefix}/tilt_corrected.swc"
-    logger.debug('Creating tmp swc file')
     tmp_path = transformed_swc_key.split("/")[-1]
     morphology_to_swc(transformed_morphology, tmp_path)
-    logger.debug('Uploading transformed swc')
     s3.upload_file(Filename=tmp_path, Bucket=bucket, Key=transformed_swc_key)
-    logger.debug('Removing tmp swc file')
     os.remove(tmp_path)
 
     return {
@@ -89,18 +78,14 @@ def run_tilt(token=None):
     run_prefix = os.environ["RUN_PREFIX"]
     upright_swc_key = os.environ["UPRIGHT_SWC_KEY"]
 
-    logger.debug('Collecting inputs')
     args = collect_inputs(working_bucket,
                           run_prefix,
                           reconstruction_id,
                           upright_swc_key)
-    logger.debug('Running tilt correction')
     (tilt_correction, tilt_transform) = run_tilt_correction(**args)
-    logger.debug('Completed tilt correction')
-    logger.debug('Transforming morphology')
+
     tf_morph = tilt_transform.transform_morphology(args['morphology'])
 
-    logger.debug('Putting outputs')
     return put_outputs(working_bucket, run_prefix,
                        tilt_correction, tilt_transform, tf_morph)
 
