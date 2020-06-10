375827c52b6f98d6b13b75a57e80bd7b4c80b20c
nilegraddis <nilegraddis@gmail.com>
2020-06-10 13:42:03 -0700

    GH-162 GH-163 colocate path features tests

--- tests/feature_extractor/test_path_features.py
+++ tests/feature_extractor/test_path_features.py
@@ -4,7 +4,14 @@ import neuron_morphology.features.path as path
 from neuron_morphology.morphology import Morphology
 from neuron_morphology.constants import (
     SOMA, AXON, APICAL_DENDRITE, BASAL_DENDRITE)
-
+from neuron_morphology.feature_extractor.feature_extractor import (
+    FeatureExtractor
+)
+from neuron_morphology.feature_extractor.feature_specialization import (
+    NEURITE_SPECIALIZATIONS)
+from neuron_morphology.feature_extractor.marked_feature import specialize
+from neuron_morphology.feature_extractor.data import Data
+from neuron_morphology.morphology_builder import MorphologyBuilder
 
 class PathTestCase(unittest.TestCase):
     def setUp(self):
@@ -128,7 +135,7 @@ class TestMeanContraction(PathTestCase):
     def test_generic(self):
         self.assertAlmostEqual(
             path.mean_contraction(self.morphology),
-            0.815431533497303
+            0.86049192013635
         )
 
     def test_specific(self):
@@ -137,6 +144,37 @@ class TestMeanContraction(PathTestCase):
                 self.morphology, node_types=[APICAL_DENDRITE]),
             1.0 # only axons backtrack
         )
+
+    def test_specialized(self):
+        morphology = (
+            MorphologyBuilder()
+                .root(0, 0, 0)
+                    .basal_dendrite(1, 0, 0)
+                        .basal_dendrite(2, 0, 0)
+                            .basal_dendrite(2, 1, 0)
+                                .basal_dendrite(1, 1, 0).up()
+                                .basal_dendrite(3, 1, 0).up(2)
+                            .axon(2, -1, 0)
+                                .axon(2, -2, 0)
+                                    .axon(1, -2, 0).up()
+                                    .axon(3, -2, 0).up(5)
+                    .apical_dendrite(0, 1, 0)
+                        .apical_dendrite(0, 2, 0)
+                            .apical_dendrite(-1, 2, 0).up()
+                            .apical_dendrite(1, 2, 0)
+                .build()
+        )
+
+        cell_data = Data(morphology=morphology)
+        fe = FeatureExtractor()
+        fe.register_features([specialize(path.mean_contraction, NEURITE_SPECIALIZATIONS)])
+        feature_extraction_run = fe.extract(cell_data)
+
+        self.assertEqual(feature_extraction_run.results["all_neurites.mean_contraction"], 1)
+        self.assertEqual(feature_extraction_run.results["axon.mean_contraction"], 1)
+        self.assertEqual(feature_extraction_run.results["dendrite.mean_contraction"], 1)
+        self.assertEqual(feature_extraction_run.results["basal_dendrite.mean_contraction"], 1)
+        self.assertEqual(feature_extraction_run.results["apical_dendrite.mean_contraction"], 1)
 
 
 class TestEarlyBranchPath(PathTestCase):
@@ -158,3 +196,32 @@ class TestMaxPathDistance(PathTestCase):
             path.max_path_distance(self.morphology),
             70.36087214122114
         )
+
+    def test_specialized(self):
+        morphology = (
+            MorphologyBuilder()
+                .root(0, 0, 0)
+                    .basal_dendrite(1, 0, 0)
+                        .axon(1, -1, 0).up()
+                        .basal_dendrite(2, 0, 0)
+                            .basal_dendrite(3, 0, 0).up()
+                            .basal_dendrite(2, 1, 0).up(3)
+                    .apical_dendrite(0, 1, 0)
+                        .apical_dendrite(0, 2, 0)
+                            .apical_dendrite(-1, 2, 0).up()
+                            .apical_dendrite(1, 2, 0)
+                                .apical_dendrite(1, 3, 0)
+                .build()
+        )
+
+
+        cell_data = Data(morphology=morphology)
+        fe = FeatureExtractor()
+        fe.register_features([specialize(path.max_path_distance, NEURITE_SPECIALIZATIONS)])
+        feature_extraction_run = fe.extract(cell_data)
+
+        self.assertEqual(feature_extraction_run.results["all_neurites.max_path_distance"], 4)
+        self.assertEqual(feature_extraction_run.results["axon.max_path_distance"], 2)
+        self.assertEqual(feature_extraction_run.results["dendrite.max_path_distance"], 4)
+        self.assertEqual(feature_extraction_run.results["basal_dendrite.max_path_distance"], 3)
+        self.assertEqual(feature_extraction_run.results["apical_dendrite.max_path_distance"], 4)
